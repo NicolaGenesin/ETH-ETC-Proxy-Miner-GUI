48baa78714673212dd7c7e5b43f52aefc6e2b8c5
Nicola Genesin <1911genesin@gmail.com>
2017-07-12 23:31:14 +0100

    + first commit

---
+++ Miner/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Miner
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Boolean isMining = false;
+        Process process = new Process();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ethWalletAddress.Text = "0x76567212871cc11ef1be9b1f93e87d96a625d1co";
+            etcWalletAddress.Text = "0x76567212871cc11ef1be9b1f93e87d96a625d1co";
+            email.Text = "test@gmail.com";
+            minerName.Text = "rig";
+            isETC.IsChecked = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isMining)
+            {
+                isMining = false;
+                ((Button)sender).Content = "Start";
+                process.Kill();
+            }
+            else
+            {
+                isMining = true;
+                ((Button)sender).Content = "Stop";
+
+                Task.Delay(1000).ContinueWith(_ =>
+                {
+                    string cryptoTarget = isETC.IsChecked.Value ? "etc" : "eth";
+                    string addressTarget = isETC.IsChecked.Value ? etcWalletAddress.Text : ethWalletAddress.Text;
+
+                    process.StartInfo.FileName = @"Stub.exe";
+                    process.StartInfo.Arguments = @"-X -epool " + cryptoTarget + "-eu1.nanopool.org:19999 -ewal " + addressTarget + "/" + minerName.Text + "/" + email.Text + " -mode 1";
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.Start();
+                    process.WaitForExit();
+                });
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Ethereum-GUI-Miner-Claymore-
+## ETH/ETC Proxy Miner GUI
+
+GUI that proxies different eth/etc miners.
+
+Miners are NOT included. Win only atm (WPF application).
+
+#### DEV
+
+1) Get geth (https://github.com/ethereum/go-ethereum/wiki/geth) and wait for the blockchain to be synchronized.
+
+2) Build, run, provide:
+
+- Email
+- Miner Name
+- ETC/ETH Address
+
+Miner defaults to eth.nanopool and etc.nanopool (Fee is 1%)
+
+#### TODO
+
+- Support Genoil
+- Support Stratum Proxy v0.0.5 by dwarfpool
+- Support QtMiner
+- Hardware settings (fan, pl, tmps, freqs, vl)
+- Redirect output to Dash
